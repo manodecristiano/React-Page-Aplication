bf6380f651cadcade2406aa5f63fbf2dcf63c918
manodecristiano <cristian.carre.online@gmail.com>
2022-05-02 23:32:19 +0200

    Testing UseRef

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import { useRef } from 'react';
 import './App.css';
 
 function App() {
+
+  const inputRef = useRef(null);
+  const resultRef = useRef(null);
+  
+  const makeThings = () => {
+    resultRef.current.innerHTML = inputRef.current.value;
+    alert(inputRef.current.value);
+   
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      <input type="text" ref={inputRef}/>
+      <button onClick={makeThings}>Hacer Cosas</button>
+   
+      <div ref={resultRef}></div>
+
     </div>
+
   );
+
+
 }
 
 export default App;
