78a6b0a55a125296af79f190dc0f707f2457c318
manodecristiano <cristian.carre.online@gmail.com>
2022-05-03 10:49:04 +0200

    The most commond USE

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import { useRef } from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { StyleSheetContext } from 'styled-components';
+
 import './App.css';
 
 function App() {
 
-  const inputRef = useRef(null);
-  const resultRef = useRef(null);
+  const [number, setNumber] = useState(8);
+  const inputRef            = useRef(null);
+  const resultRef           = useRef(null);
+  const [txt, setTxt]       = useState('')
+  
+
+  useEffect(() => {
+    alert('Cambio el estado de number');
+  },[number]);
+
   
   const makeThings = () => {
     resultRef.current.innerHTML = inputRef.current.value;
     alert(inputRef.current.value);
-   
   }
+
+  const changeNum = () => {
+    
+    setNumber(5);
+  }
+
+  const textChange = (evento) => {
+    setTxt(evento.target.value);
+
+  }
+
+
   return (
 
     <div>
+     
       <input type="text" ref={inputRef}/>
-      <button onClick={makeThings}>Hacer Cosas</button>
-   
+      <button onClick={makeThings}> Imprimir en pantalla </button>
       <div ref={resultRef}></div>
+     
+      <button onClick={() => changeNum()}> Cambiar Número </button>
+      {number}
+
+      <input type="text" onChange={(evento) => textChange(evento)}/>
+      {txt}
+
+    
 
     </div>
 
